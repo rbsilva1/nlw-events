cdc0e1fe461077e0d4b98a484675bfa8ca65e59e
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-04-07 11:58:28 -0300

    feat: criando rota de GetEvent e RegisterForEvent

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 const app = fastify();
 
@@ -8,6 +10,8 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
 
 app.listen({ port: 3000 }).then(() => {
   console.log('HTTP Server is running');
