b53950997df976876ba25f122c6f52027939af65
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-04-06 22:21:14 -0300

    feat: create modules to reorganize the app

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import fastify from "fastify";
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
+
 app.listen({ port: 3000 }).then(() => {
-  console.log("HTTP server running!");
+  console.log('HTTP Server is running');
 })
